24da15599a61b67503749b1bf11a2addd25964c0
unknown <marcusviniciusdasilveiraricha@gmail.com>
2019-01-27 20:34:19 -0200

    1o Unit Test and URL mapping, with dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,22 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-# Create your tests here.
+"""
+# Apenas testando se o teste funciona
+# py manage.py test
 class SmokeTest(TestCase):
 	def test_bad_maths(self):
 		self.assertEqual(1 + 1, 3)
+"""
+
+# Create your tests here.
+class HomePageTest(TestCase):
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
+
+
 
 
 
--- superlists/urls.py
+++ superlists/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from lists import views
+
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
+		# RE representativa de uma string vazia
+		url(r'^$', views.home_page, name='home'),
 ]
