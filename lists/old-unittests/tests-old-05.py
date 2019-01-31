1cf31dd95e07ad6af7480ee7b653b3d87e3582c9
unknown <marcusviniciusdasilveiraricha@gmail.com>
2019-01-31 21:18:19 -0200

    Inserting new_list functionality: class test, url, view. And refactoring meth home_page and class test HomePageTest

--- lists/old-unittests/tests-old-05.py
+++ lists/old-unittests/tests-old-05.py
@@ -7,13 +7,28 @@ class HomePageTest(TestCase):
 	def test_uses_home_template(self):
 		response = self.client.get('/')
 		self.assertTemplateUsed(response, 'home.html')
+	
+	def test_can_save_a_POST_request(self):
+		self.client.post('/', data={'item_text': 'A new list item'})
 		
-	"""
-		Com a separação das classes e as refatorações, este teste deixou de ser necessário:
+		self.assertEqual(Item.objects.count(),1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+	
+	def test_redirects_after_POST(self):
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertEqual(response.status_code, 302)
+		
+		# mudando o redirecionamento em função do design multi-user
+		#self.assertEqual(response['location'], '/')
+		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
+	
 	def test_only_saves_items_when_necessary(self):
 		self.client.get('/')
 		self.assertEqual(Item.objects.count(), 0)
 		
+	"""
+		Este teste deixou de ser necessário
 	def test_displays_all_list_items(self):
 		Item.objects.create(text='itemey 1')
 		Item.objects.create(text='itemey 2')
@@ -24,22 +39,7 @@ class HomePageTest(TestCase):
 		self.assertIn('itemey 1', response.content.decode())
 		self.assertIn('itemey 2', response.content.decode())
 	"""
-
-class NewListTest(TestCase):
-	def test_can_save_a_POST_request(self):
-		self.client.post('/', data={'item_text': 'A new list item'})
 		
-		self.assertEqual(Item.objects.count(),1)
-		new_item = Item.objects.first()
-		self.assertEqual(new_item.text, 'A new list item')
-	
-	def test_redirects_after_POST(self):
-		# Atenção: pela conveção, URLs sem barras ao final são URLs de "ação" que modificam o db
-		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-		
-		# Condensando aqui dois méts assertEqual() por um assertRedirects()
-		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
-	
 class ItemModelTest(TestCase):	
 	def test_saving_and_retrieving_items(self):
 		first_item = Item()
--- lists/views-old.py
+++ lists/views-old.py
@@ -6,7 +6,14 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-	return render(request, 'home.html')
+	#return HttpResponse('<html><title>To-Dolists</title></html>')
+	
+	if request.method == 'POST':
+		Item.objects.create(text=request.POST['item_text'])
+		return redirect('/lists/the-only-list-in-the-world/')
+	
+	items = Item.objects.all()
+	return render(request, 'home.html', {'items': items})
 	
 def view_list(request):
 	items = Item.objects.all()
