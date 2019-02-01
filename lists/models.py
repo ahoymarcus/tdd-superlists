5c847921f9569d79701135095e5557309ccb1f26
unknown <marcusviniciusdasilveiraricha@gmail.com>
2019-01-31 23:31:54 -0200

    Adding new model List and adjunsting test.py and views.py

--- lists/models.py
+++ lists/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 
 # Create your models here.
+# Veja que como o modelo Item insere chave estrangeira daqui na forma de objeto, este modelo, então, tem de vir primeiro
+class List(models.Model):
+	pass
+	
 class Item(models.Model):
 	text = models.TextField(default='')
+	
+	# Veja que o objetivo não é criar uma coluna de texto 'list', simplesmente, mas criar um relacionamento de chave estrangeira nesta coluna
+	#list = models.TextField(default='')
+	list = models.ForeignKey(List, default=None)
+	
 
 
 
