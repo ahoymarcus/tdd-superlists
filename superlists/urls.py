54d276cf2e67ccc6ecfec61635b2b6470c1f8988
unknown <marcusviniciusdasilveiraricha@gmail.com>
2019-02-01 00:59:57 -0200

    new URL + view for adding to existing lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,11 +18,19 @@ from django.contrib import admin
 
 from lists import views
 
-
+"""
+	Observe que na criação da URL para view_list precisamos trocar uma RE mais gulosa r'^lists/(.+)/$', para uma mais específica
+"""
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
 		# RE representativa de uma string vazia
 		url(r'^$', views.home_page, name='home'),
 		url(r'^lists/new$', views.new_list, name='new_list'),
-		url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+		url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+		url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
 ]
+
+
+
+
+
